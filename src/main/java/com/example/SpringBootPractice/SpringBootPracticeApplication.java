aba1be70944e24b865d68a587865ec9fa5749b55
vrath <vrath@Virendra-Yoga.phub.net.cable.rogers.com>
2021-03-29 18:52:09 -0400

    This is the SpringBootPractice

---
+++ src/main/java/com/example/SpringBootPractice/SpringBootPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringBootPractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+//@RestController
+public class SpringBootPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootPracticeApplication.class, args);
+    }
+}
